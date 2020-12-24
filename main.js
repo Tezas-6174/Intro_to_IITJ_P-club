8bfe502ac3046b7a358a01a7782ec7b3ae109566
Tejas Jamdade <75741089+Tezas-6174@users.noreply.github.com>
2020-12-24 20:05:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'scripts/images/i.jpg') {
+      myImage.setAttribute('src','scripts/images/j.png');
+    } else {
+      myImage.setAttribute('src','scripts/images/i.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
